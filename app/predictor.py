937f76dc3ebd9b11bec99312dcc663c3b2f657d1
Sivakumar M <mailtoosivakumar@gmail.com>
2025-05-04 01:16:40 +0530

    update trineed module path

--- app/predictor.py
+++ app/predictor.py
@@ -2,7 +2,7 @@ import joblib
 import pandas as pd
 
 def load_model():
-    return joblib.load("../models/resale_price_model.pkl")
+    return joblib.load("models/resale_price_model.pkl")
 
 def make_prediction(model, user_input):
     df = pd.DataFrame([user_input])
