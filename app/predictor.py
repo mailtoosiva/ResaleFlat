da6b9e1f5b5f43379ea9f6263b9eb2f12072efe5
Sivakumar M <mailtoosivakumar@gmail.com>
2025-05-04 13:02:48 +0530

    File added in hugging face

--- app/predictor.py
+++ app/predictor.py
@@ -1,9 +1,33 @@
+import os
 import joblib
-import pandas as pd
+import requests
 
 def load_model():
-    return joblib.load("models/resale_price_model.pkl")
+    model_path = "models/resale_price_model.pkl"
+    if not os.path.exists(model_path):
+        os.makedirs("models", exist_ok=True)
+        url = "https://huggingface.co/mailtoosiva/resale-price-predictor/resolve/main/models/resale_price_model.pkl"
+        print("Downloading model from Hugging Face...")
+        r = requests.get(url)
+        with open(model_path, 'wb') as f:
+            f.write(r.content)
+    return joblib.load(model_path)
 
-def make_prediction(model, user_input):
-    df = pd.DataFrame([user_input])
-    return model.predict(df)[0]
+
+
+
+
+
+
+
+
+
+# import joblib
+# import pandas as pd
+
+# def load_model():
+#     return joblib.load("models/resale_price_model.pkl")
+
+# def make_prediction(model, user_input):
+#     df = pd.DataFrame([user_input])
+#     return model.predict(df)[0]
