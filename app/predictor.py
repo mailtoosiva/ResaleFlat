311bd997a43499939c978980d1c10df94b878750
Sivakumar M <mailtoosivakumar@gmail.com>
2025-05-04 14:22:46 +0530

    update logs

--- app/predictor.py
+++ app/predictor.py
@@ -3,19 +3,21 @@ import joblib
 import requests
 import pandas as pd
 
+
 def load_model():
     model_path = "models/resale_price_model.pkl"
     if not os.path.exists(model_path):
+        print("Model not found locally. Downloading from Hugging Face...")
         os.makedirs("models", exist_ok=True)
         url = "https://huggingface.co/mailtoosiva/resale-price-predictor/resolve/main/models/resale_price_model.pkl"
-        r = requests.get(url, stream=True)
-        if r.status_code != 200:
-            raise Exception(f"Download failed: {r.status_code}")
-        with open(model_path, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=8192):
-                if chunk:
-                    f.write(chunk)
+        response = requests.get(url)
+        if response.status_code == 200:
+            with open(model_path, 'wb') as f:
+                f.write(response.content)
+        else:
+            raise Exception(f"Failed to download model. HTTP Status Code: {response.status_code}")
     return joblib.load(model_path)
+
 
 
 def make_prediction(model, user_input):
